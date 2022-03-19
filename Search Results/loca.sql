ca395a5438bd1c3a381484807443ef3ee01bfe2f
Leo Gavin <leogavin123@outlook.com>
2022-03-19 14:11:56 +0530

    Update loca.sql

--- Search Results/loca.sql
+++ Search Results/loca.sql
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `locations` (
   `id` int(11) NOT NULL,
   `country_name` varchar(191) NOT NULL,
   `location_name` varchar(100) NOT NULL,
@@ -38,23 +38,88 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `country_name`, `location_name`, `AboutLocation`) VALUES
-(1, '<!--Paris-->', '<!--Eiffel Tower-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(2, '<!--China-->', '<!--Great wall Of China-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(3, '<!--Italy-->', '<!--The Leaning tower of pisa-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(4, '<!--Sri Lanka-->', '<!--Colombo-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(5, '<!--Sri Lanka-->', '<!--kandalama-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(6, '<!--Sri Lanka-->', '<!--Sigiriya-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(7, '<!--Sri Lanka-->', '<!--Pinnawala Elephant Orphanage-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(8, '<!--Bolivia-->', '<!--Salar De Uyuni-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(9, '<!--Australia-->', '<!--the gret barrier Reaf-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(10, '<!--Philipines-->', '<!--Palawan island-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(11, '<!--New York City-->', '<!--Staus Of Liberty-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(12, '<!--Peru South America-->', '<!--Machu picchu-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(13, '<!--India-->', '<!--The Taj Mahal-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(14, '<!--Egypt-->', '<!--Pyramid of giza-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(15, '<!--Sri Lanka-->', '<!--Mirissa-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> '),
-(16, '<!--Canada-->', '<!--Niagara Falls-->', '<a href=\"https://www.w3schools.com\">Please login to this site </a> ');
+INSERT INTO `locations` (`id`, `country_name`, `location_name`, `AboutLocation`) VALUES
+(1, '<!--Paris-->', '<!--Eiffel Tower-->', '<section class="search-result-item">
+    <a class="image-link" href="#"><img class="image"
+            src="Images/Eiffeltower.jpg-1.png">
+    </a>
+    <div class="search-result-item-body">
+        <div class="row">
+            <div class="col-sm-9">
+                <h4 class="search-result-item-heading"><a href="#">Eiffel Tower</a></h4>
+                <p class="info">Paris</p>
+                <p class="description">The Eiffel Tower was built for the 1889 Exposition Universelle in Paris. Its
+                    construction in 2 years, 2 months and 5 days was a veritable technical and architectural
+                    achievement. Today it welcomes almost 7 million visitors a year, making it the most visited monument
+                    in the world.
+
+                </p>
+            </div>
+            <div class="col-sm-3 text-align-center">
+            </br> </br></br>
+
+                <a class="btn btn-primary btn-info btn-sm" href="#">Learn More</a>
+            </div>
+        </div>
+    </div>
+</section>'),
+(2, '<!--China-->', '<!--Great wall Of China-->', '<section class="search-result-item">
+    <a class="image-link" href="#"><img class="image"
+            src="Images/greatwall.png">
+    </a>
+    <div class="search-result-item-body">
+        <div class="row">
+            <div class="col-sm-9">
+                <h4 class="search-result-item-heading"><a href="#">Great wall Of China</a></h4>
+                <p class="info">China</p>
+                <p class="description">The Great Wall of China stretches over 4,000 kilometers (2,500 miles). The Jinshanling section in Hebei Province, China is only a small part of the wall. It is one of the most famous structures in the entire world.
+
+
+                </p>
+            </div>
+            <div class="col-sm-3 text-align-center">
+            </br> </br></br>
+
+                <a class="btn btn-primary btn-info btn-sm" href="#">Learn More</a>
+            </div>
+        </div>
+    </div>
+</section>'),
+(3, '<!--Italy-->', '<!--The Leaning tower of pisa-->', '<section class="search-result-item">
+    <a class="image-link" href="#"><img class="image"
+            src="Images/pisa.png">
+    </a>
+    <div class="search-result-item-body">
+        <div class="row">
+            <div class="col-sm-9">
+                <h4 class="search-result-item-heading"><a href="#">The Leaning tower of pisa</a></h4>
+                <p class="info">Italy</p>
+                <p class="description">After the cathedral and the Pisa Baptistry, the tower is the third-oldest structure in the citys Cathedral Square (Piazza del Duomo). The tower increases 55.86 meters (183 feet 3 inches) from the ground on the low side and 56.67 meters (185 feet 11 inches) on the high side.
+
+
+                </p>
+            </div>
+            <div class="col-sm-3 text-align-center">
+            </br> </br></br>
+
+                <a class="btn btn-primary btn-info btn-sm" href="#">Learn More</a>
+            </div>
+        </div>
+    </div>
+</section>'),
+(4, '<!--Sri Lanka-->', '<!--Colombo-->', ''),
+(5, '<!--Sri Lanka-->', '<!--kandalama-->', ''),
+(6, '<!--Sri Lanka-->', '<!--Sigiriya-->', ' '),
+(7, '<!--Sri Lanka-->', '<!--Pinnawala Elephant Orphanage-->', ''),
+(8, '<!--Bolivia-->', '<!--Salar De Uyuni-->', ''),
+(9, '<!--Australia-->', '<!--the gret barrier Reaf-->', ''),
+(10, '<!--Philipines-->', '<!--Palawan island-->', ''),
+(11, '<!--New York City-->', '<!--Staus Of Liberty-->', ''),
+(12, '<!--Peru South America-->', '<!--Machu picchu-->', ''),
+(13, '<!--India-->', '<!--The Taj Mahal-->', ''),
+(14, '<!--Egypt-->', '<!--Pyramid of giza-->', ''),
+(15, '<!--Sri Lanka-->', '<!--Mirissa-->', ''),
+(16, '<!--Canada-->', '<!--Niagara Falls-->', '');
 
 
 --
@@ -64,7 +129,7 @@ INSERT INTO `users` (`id`, `country_name`, `location_name`, `AboutLocation`) VAL
 --
 -- Indexes for table `users`
 --
-ALTER TABLE `users`
+ALTER TABLE `locations`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -74,7 +139,7 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `users`
+ALTER TABLE `locations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
